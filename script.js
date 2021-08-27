858a11263e9f2a43135cb378f6cd1dc5157a986b
Ivan Lesko <65163471+Ivanlsko@users.noreply.github.com>
2021-08-27 08:39:35 +0200

    button podpora v1

--- script.js
+++ script.js
@@ -5,39 +5,70 @@ const sprievodcaText = [
   "stvrty subor detailnejsich usmernujucich pokynov pre stredne pokrocilych pouzivatelov",
 ];
 
-window.addEventListener("load", logIt);
-
-function logIt() {
-  console.log(sprievodcaText);
-}
-
+const sprievodcaContent = document.querySelector(".sprievodcaContent");
+let position = 0;
+const next = document.querySelector(".nextPos");
+const back = document.querySelector(".backPos");
+let backIsHidden = false;
+let nextIsHidden = false;
 const btnSprievodca = document.querySelector(".btnSprievodca");
 const sprievodca = document.querySelector(".sprievodca");
+const podpora = document.querySelector(".podpora");
+const btnPodpora = document.querySelector(".btnPodpora");
+
+/*<---------- sprievodca opener--------->*/
 
 btnSprievodca.addEventListener("click", openSprievodca);
 
 function openSprievodca() {
+  if (backIsHidden == false) {
+    back.classList.add("hidden");
+    backIsHidden = true;
+  }
   sprievodca.classList.toggle("hidden");
+  document.querySelector("#pos0").classList.toggle("positionFull");
 }
+
 /*<---------- slideshow --------->*/
 
-const sprievodcaContent = document.querySelector(".sprievodcaContent");
-let position = 0;
 sprievodcaContent.textContent = sprievodcaText[position];
 
-const next = document.querySelector(".nextPos");
-const back = document.querySelector(".backPos");
-
 next.addEventListener("click", nextPos);
-
+back.addEventListener("click", backPos);
 function nextPos() {
   sprievodcaContent.textContent = sprievodcaText[position + 1];
+  document.querySelector("#pos" + position).classList.remove("positionFull");
   position = position + 1;
-};
-
-back.addEventListener("click", backPos);
+  document.querySelector("#pos" + position).classList.add("positionFull");
+  if (position > 0) {
+    back.classList.remove("hidden");
+  }
+  if (position == 3) {
+    next.classList.add("hidden");
+    nextIsHidden = true;
+  }
+}
 
 function backPos() {
   sprievodcaContent.textContent = sprievodcaText[position - 1];
+  document.querySelector("#pos" + position).classList.remove("positionFull");
   position = position - 1;
+  document.querySelector("#pos" + position).classList.add("positionFull");
+  if (position == 0) {
+    back.classList.add("hidden");
+    backIsHidden = true;
+  }
+
+  if (nextIsHidden == true) {
+    next.classList.remove("hidden");
+    nextIsHidden = false;
+  }
+}
+
+/*<---------- podpora opener--------->*/
+
+btnPodpora.addEventListener("click", openPodpora);
+
+function openPodpora() {
+  podpora.classList.toggle("hidden");
 }
