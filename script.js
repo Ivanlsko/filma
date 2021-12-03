a3ef4959b8d71b5f1704f5e1159252dc596ca234
Ivan Lesko <65163471+Ivanlsko@users.noreply.github.com>
2021-12-03 11:41:26 +0100

    close button script 1

--- script.js
+++ script.js
@@ -20,10 +20,17 @@ function determineBtn() {
 
   if (this.innerText == "nahrať") {
     nahrať();
+    manageContent();
   }
 
   if (this.innerText == "podpora") {
     podpora();
+    manageContent();
+  }
+
+  if (this.innerText == "zavrieť") {
+    zavrieť();
+    manageContent();
   }
 }
 
@@ -40,6 +47,10 @@ function podpora() {
   console.log("podpora function");
 }
 
+function zavrieť() {
+  console.log("podpora function");
+}
+
 // Content management functions
 
 function manageContent() {
@@ -50,8 +61,11 @@ function manageContent() {
 function clearCon() {
   //remove all the stuff
   const header = document.querySelector("header");
+  //make it hidden so it doesnt mess with layout
   header.children[1].style.visibility = "hidden";
   header.children[2].style.visibility = "hidden";
+  //make the content elements display none - to allow formating of the new content
+  setTimeout(hiddenToNone(header), 300);
 
   //change BG color - CSS animation
   document.querySelector("#secondaryBg").classList.add("blackToGreen");
@@ -59,11 +73,21 @@ function clearCon() {
 
 function arrangeBtns() {
   //arange the btns (use absolute on btn and margin-left: -30vw on #buttons)
+  document.querySelector("#buttons").classList.add("opacityZero");
+  document.querySelector(".btnSprievodca").classList.add("rightToLeftSprievodca");
+  document.querySelector(".btnNahrat").classList.add("rightToLeftNahrat");
 
   //after arranging the buttons show to the close button
-  setTimeout(showCloseBtn, 3000);
+  setTimeout(showCloseBtn, 200);
 }
 
 function showCloseBtn() {
   console.log("currentEnd");
+  document.querySelector(".btnZavriet").classList.add("opacityOne");
+}
+
+function hiddenToNone(parent) {
+  parent.children[1].style.display = "none";
+  parent.children[2].style.display = "none";
+  parent.children[3].style.display = "none";
 }
